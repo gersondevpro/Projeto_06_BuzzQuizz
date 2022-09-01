289cd32e4b3d07e6784f3b4e74026e489f050626
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-08-31 23:01:19 -0300

    Inicio da função quizz escolhido

--- scrip.js
+++ scrip.js
@@ -1,5 +1,6 @@
 let listaQuizzes =[];
-
+const valorId =[];
+// Busca todos os quizzes no servidor
 function buscarQuizzes(){
     const pedido = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
     pedido.then(renderizarQuizzes);
@@ -7,15 +8,18 @@ function buscarQuizzes(){
 }
 buscarQuizzes();
 
+
+// Adiciona os quizzes na parte do todos
+
 function renderizarQuizzes(resposta){
 
     listaQuizzes = resposta.data 
 
- const quizz =document.querySelector('.listaQuizzes');
+ const quizz =document.querySelector('.todos');
 
  quizz.innerHTML= ""
  for(let i=0; i<listaQuizzes.length; i++){
-    quizz.innerHTML += `<div class="itemListaMain" onclick="abrirQuizz('${listaQuizzes[i].id}',this)">
+    quizz.innerHTML += `<div class="itemListaMain" onclick="abrirQuizz('${listaQuizzes[i].id}')">
     <div class="relativeIndex">
 
         <div class="gradient"></div>
@@ -35,10 +39,14 @@ function TratarErro(erro){
     console.log(erro.response.data)
 }
 
-function abrirQuizz (id,quizzEscolhido){
+// seleciona o id do quizz clicado e busca a classe da tela 1
 
-    const valorId = Number(id)
-    const quizz = quizzEscolhido
-    console.log(valorId)
+function abrirQuizz (id){
+
+    valorId = Number(id);
+    const tela1 = document.querySelector('.tela1');
+    tela1.classList.add('hidden');
+    const tela2 =document.querySelector('.tela2');
+    tela2.classList.remove('hidden');
 }
-
+ 
